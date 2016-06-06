c4ec34bc3ad9b112e28a65d44c8eb764faf81094
Tatsuya Nakamura <al1.1ion.nkmr@gmail.com>
2016-06-06 13:10:13 +0900

    [tutorial1] Added CommentBox component

---
+++ src/app/CommentBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div className='commentBox'>
+        <h1>Hello, world! I am a CommentBox.</h1>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
--- src/app/app.js
+++ src/app/app.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import CommentBox from './CommentBox'
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<h1>Hi!</h1>, document.getElementById('app'));
+render(<CommentBox />, document.getElementById('app'));
