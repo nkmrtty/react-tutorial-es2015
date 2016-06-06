bfe839782541d02dcd9ed1ab6ba4210b4b42a004
Tatsuya Nakamura <al1.1ion.nkmr@gmail.com>
2016-06-06 12:41:57 +0900

    Removed an unnecessary import statement

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
